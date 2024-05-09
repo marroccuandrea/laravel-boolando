df5a82768889768a7465c03414903aec47c30f79
marroccuandrea <marroccuandrea@yahoo.it>
2024-05-09 18:32:16 +0200

    Card dinamiche

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,36 @@
 @extends('layout.main')
 
 @section('content')
-    <h1>Home</h1>
+    @php
+        $cards = config('productcards');
+    @endphp
+    <main>
+        <div class="container">
+
+            @foreach ($cards['products'] as $item)
+                <div class="card">
+                    <div class="card-images">
+                        <img src="{{ Vite::asset('public/img/') . $item['frontImage'] }}" alt="">
+                        {{-- <img class="secondary-image" :src="`/img/${vestito.backImage}`" alt="1"> --}}
+                        <div class="favourite">
+                            <i class="fa-solid fa-heart"></i>
+                        </div>
+                        <div class="badges">
+                            @foreach ($item['badges'] as $badge)
+                                <span class="{{ $badge['type'] }}">{{ $badge['value'] }}</span>
+                            @endforeach
+                        </div>
+                    </div>
+
+                    <div class="card-text">
+                        <div class="brand">{{ $item['brand'] }}</div>
+                        <div class="product-name">{{ $item['name'] }}</div>
+                        <div class="price">
+                            <span>{{ $item['price'] }} &#8364</span>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </main>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -18,11 +18,11 @@
                 <nav>
                     <ul>
                         <li>
-                            <a>icona</a>
-                            <a>icona</a>
-                            <a>icona</a>
-                            <a>icona</a>
-                            <a>icona</a>
+                            <a><i class="fa-brands fa-square-x-twitter"></i></a>
+                            <a><i class="fa-brands fa-square-facebook"></i></a>
+                            <a><i class="fa-brands fa-instagram"></i></a>
+                            <a><i class="fa-brands fa-pinterest"></i></a>
+                            <a><i class="fa-brands fa-youtube"></i></a>
                         </li>
                     </ul>
                 </nav>
